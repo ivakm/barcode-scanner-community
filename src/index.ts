3f48d09796788c2d5eef46f8c8b478cb155f9ed3
Dmytro Shevchenko <dmytro.shevchenko@e-hps.com>
2023-05-30 08:56:12 -0400

    removed ios

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { BarcodeScannerPlugin } from './definitions';
 
-const BarcodeScanner = registerPlugin<BarcodeScannerPlugin>('BarcodeScanner', {
-  web: () => import('./web').then(m => new m.BarcodeScannerWeb()),
+const BarcodeScannerCommunity = registerPlugin<BarcodeScannerPlugin>('BarcodeScannerCommunity', {
+  web: () => import('./web').then((m) => new m.BarcodeScannerWeb()),
 });
 
 export * from './definitions';
-export { BarcodeScanner };
+export { BarcodeScannerCommunity };
